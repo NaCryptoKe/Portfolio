ddc235352d885742b70fd71d7668f285d01813ce
NaCryptoKe <Nahomcryptoketema@gmail.com>
2025-08-08 09:03:38 +0300

    First propper commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thanks for reaching out! Unfortunately, this form is just for show right now 😅");
+});
